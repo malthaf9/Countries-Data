838ea2bf6001dae404ec1d31b99c3957fa1d1655
Altaf Hussain <malthaf989@gmail.com>
2024-07-22 18:08:51 +0530

    Update Body.js

--- src/components/Body.js
+++ src/components/Body.js
@@ -4,17 +4,18 @@ import Filter from "./Filter";
 import CountryContainer from "./CountryContainer";
 
 const Body = () => {
-    const [query, setQuery] = useState('')
-    const [region, setRegion] = useState('')
-    return(
-        <div className="">
-            <div className="flex justify-between p-4">
-            <Search setQuery={setQuery} />
-            <Filter setRegion={setRegion} />
-            </div>
-            <CountryContainer query={query} region={region} />
-        </div>
-    )
-}
+  const [query, setQuery] = useState('');
+  const [region, setRegion] = useState('');
 
-export default Body
+  return (
+    <div className="">
+      <div className="flex flex-col md:flex-row justify-between p-4 space-y-4 md:space-y-0 md:space-x-4">
+        <Search setQuery={setQuery} />
+        <Filter setRegion={setRegion} />
+      </div>
+      <CountryContainer query={query} region={region} />
+    </div>
+  );
+};
+
+export default Body;
